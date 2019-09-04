6eb404fdd923d56467c6946f9402c2a3ce870004
marc schneider <marc_schneider@gmx.ch>
2019-09-04 11:03:00 +0200

    first prototype - fully functional but without db

---
+++ hashing.py
@@ -0,0 +1,10 @@
+import hashlib
+
+
+def hash_calculation(filename):
+    sha256_hash = hashlib.sha256()
+    with open(filename, "rb") as f:
+        for byte_block in iter(lambda: f.read(4096), b""):
+            sha256_hash.update(byte_block)
+        hash_hex = sha256_hash.hexdigest()
+        global hash_hex
--- main.py
+++ main.py
@@ -1,4 +1,18 @@
 import db
-import hashlib
+import hashing
+
+import os
 
 db.create_table()
+
+
+hashpath = input("Enter Path of the file, you want to observe (e.g. Host-File in C:\Windows\System32\drivers\etc): ")
+filename = input("Enter the name of the File: ")
+hashpath_joined = os.path.join(hashpath)
+
+try:
+    os.chdir(hashpath_joined)
+    hash_value = hashing.hash_calculation(filename)
+    print(hash_value)
+except IOError:
+    print("Typo in file-path or file does not exist")
