68543ffe82f84fc2c672fd171ad31c4c321f4dfc
marc schneider <marc_schneider@gmx.ch>
2019-09-23 13:46:08 +0200

    fixed missing filename problem

--- main.py
+++ main.py
@@ -6,19 +6,28 @@ import os
 db.create_table()
 
 
-user_input_path = input("Enter Path of the file, you want to observe "
-                        "(e.g. Host-File in C:\\Windows\\System32\\drivers\\etc): ")
-filename = input("Enter the name of the File: ")
-hashpath_joined = os.path.join(user_input_path)
+def add_file():
+    user_input_path = input("Enter Path of the file, you want to observe "
+                            "(e.g. Host-File in C:\\Windows\\System32\\drivers\\etc): ")
+    filename = input("Enter the name of the File: ")
 
-try:
-    os.chdir(hashpath_joined)
-    hash_value = hashing.hash_calculation(filename)
-    hashed_file = hashpath_joined + "\\" + filename
-    print("Hashvalue of " + hashed_file + " is " + hash_value)
-    db.insert_data(hashed_file, hash_value)
-except IOError:
-    print("Typo in file-path or file does not exist")
+    if not filename:
+        user_input_path.strip()
+        hashpath_joined = os.path.join(user_input_path)
+    else:
+        hashpath_joined = os.path.join(user_input_path)
+
+    try:
+        os.chdir(hashpath_joined)
+        hash_value = hashing.hash_calculation(filename)
+        hashed_file = hashpath_joined + "\\" + filename
+        print("Hashvalue of " + hashed_file + " is " + hash_value)
+
+        # Add entries to db-file
+        db.insert_data(hashed_file, hash_value)
+
+    except IOError:
+        print("Typo in file-path or file does not exist")
 
 
 def compare():
